03aba3f2110f470b076043bf42a7ca90b1baef9e
Matiasahumada <pagosptcmatias@gmail.com>
2024-09-21 12:33:55 -0300

    movi componentes de sidebar a admin

--- front/src/components/admin/DashboardAdmin.jsx
+++ front/src/components/admin/DashboardAdmin.jsx
@@ -1,10 +1,11 @@
+import SidebarAdmin from "../Context/SidebarAdmin"
+import TablesUser from "./TablesUser"
 
 const DashboardAdmin = () => {
   return (
-    <div>
-      <h2 className="mb-4 text-2xl font-bold">Dashboard del Administrador</h2>
-      <p>Aquí esta el dashboard del admin</p>
-    </div>
+    <>
+      <TablesUser></TablesUser>
+    </>
   )
 }
 
