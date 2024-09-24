6a023b4f9a5bfa153fb2696f5bb729132d44097d
Matiasahumada <pagosptcmatias@gmail.com>
2024-09-23 21:18:34 -0300

    configure cors y realice una prueba desde el allusers

--- front/src/components/Users/AllUsers.jsx
+++ front/src/components/Users/AllUsers.jsx
@@ -1,45 +1,63 @@
-import {Table,Button, Container} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import { Table, Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { getUsers } from "../service/queris";
 const AllUsers = () => {
+  useEffect(() => {
+    getUsers().then((r) => {
+      console.log(r.data);
+    });
+  }, []);
   return (
     <Container>
-    <h1 className='text-center'>Clientes</h1>
-    <Table striped bordered hover variant="dark">
-        
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>DNI</th>
-        <th>ID Cred.</th>
-        <th>Funtions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>123</td>
-        <td>42499732</td>
-        <td>1,2,3</td>
-        <td><Button as={Link} to="/admin/user">Ver Más</Button></td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>1,2,3</td>
-        <td><Button as={Link} to="/admin/user">Ver Más</Button></td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td >Larry the Bird</td>
-        <td>12/10/24</td>
-        <td>1,2,3</td>
-        <td><Button as={Link} to="/admin/user">Ver Más</Button></td>
-      </tr>
-    </tbody>
-  </Table>
-  </Container>
+      <h1 className="text-center">Clientes</h1>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>DNI</th>
+            <th>ID Cred.</th>
+            <th>Funtions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>123</td>
+            <td>42499732</td>
+            <td>1,2,3</td>
+            <td>
+              <Button as={Link} to="/admin/user">
+                Ver Más
+              </Button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>1,2,3</td>
+            <td>
+              <Button as={Link} to="/admin/user">
+                Ver Más
+              </Button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Larry the Bird</td>
+            <td>12/10/24</td>
+            <td>1,2,3</td>
+            <td>
+              <Button as={Link} to="/admin/user">
+                Ver Más
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 
--- front/src/components/service/queris.js
+++ front/src/components/service/queris.js
@@ -5,7 +5,7 @@ const URL = "http://localhost:3000"
 //USERS
 export const getUsers = async () => {
     try {
-      const users = axios.get(URL + "/users");
+      const users = axios.get(URL + "/user/");
       return users;
     } catch (error) {
       console.log(error);
