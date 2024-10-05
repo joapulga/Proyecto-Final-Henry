d82eee1bc17a9fec5b5933a59425a91aa9a03a27
jfcamacho <jefferson-camacho@hotmail.com>
2024-10-05 18:14:28 -0500

    Solved some problems

--- back/src/share/share.service.ts
+++ back/src/share/share.service.ts
@@ -4,12 +4,15 @@ import { UploadShareDto } from './dto/update-share.dto';
 import { Share } from './entities/share.entity'; //remplazar por la entidad correspondiente
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { State } from 'src/state/entities/state.entity';
+import { StatesService } from 'src/state/states.service';
 
 @Injectable()
 export class ShareService {
   constructor(
     @InjectRepository(Share)
     private readonly shareRepository: Repository<Share>, //por nombre de la entidad dentro de repository
+    private readonly stateService: StatesService
   ) {}
 
   async create(createShareDto: CreateShareDto[]): Promise<Share> {
@@ -23,6 +26,18 @@ export class ShareService {
 
   async findOne(id: string): Promise<Share | null> {
     return await this.shareRepository.findOneBy({ id });
+  }
+
+  async paidById(id: string): Promise<string> {
+    const newState = await this.stateService.findOneByName('Paid')
+    const oldShare = await this.shareRepository.findOneBy({id: id})
+    const newShare = this.shareRepository.merge(oldShare, {
+      ...oldShare, 
+      state: newState,
+      paid_date: new Date()
+    })
+    await this.shareRepository.save(newShare)
+    return newShare.id
   }
 
   async update(id: string, UploadShareDto: UploadShareDto): Promise<Share | null> {
